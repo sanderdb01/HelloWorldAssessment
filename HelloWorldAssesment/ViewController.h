e46e5b2bb698dabfde5b2b1e34749aef31647552
David Sanders <sanderdb01@gmail.com>
2014-10-09 20:52:20 -0400

    Initial Commit

---
+++ HelloWorldAssesment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorldAssesment
+//
+//  Created by David Sanders on 10/9/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorldAssesment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorldAssesment
+//
+//  Created by David Sanders on 10/9/14.
+//  Copyright (c) 2014 Bitfountain.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
